d4ff643e1c55bb9d30baa17fe7a26a7ae70d526a
Mohammad Zuha khalid <zuhakk@gmail.com>
2020-03-09 14:34:10 +0530

    Changes in package.json 2

--- server/index.js
+++ server/index.js
@@ -94,9 +94,9 @@ app.delete("/users", async (req, res) => {
 })
 
 
-app.listen(5000, () => {
-    console.log("server running on port 5000")
-})
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 
